8d98bb25f739071576fb504e1b9a12c7c1e343b9
tutajm <mtutaj@mcw.edu>
2020-12-10 21:54:45 -0600

    updated script

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -5,10 +5,15 @@ APPNAME=mutant-strain-annotation-pipeline
 APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
+EMAILLIST=mtutaj@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAILLIST="mtutaj@mcw.edu,sjwang@mcw.edu"
+fi
+
 cd $APPDIR
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
     -jar lib/${APPNAME}.jar "$@" > $APPDIR/run.log 2>&1
 
-mailx -s "[$SERVER] Mutant Strain Annotation pipeline ok" mtutaj@mcw.edu < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] Mutant Strain Annotation pipeline ok" $EMAILLIST < $APPDIR/logs/summary.log
